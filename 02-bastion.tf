f5486f6e13b645b4b6d40b86d6eb5d9c2270cbd3
Rupesh Panwar <75510135+rupeshpanwar@users.noreply.github.com>
2021-08-31 14:16:22 +0530

    Update 02-bastion.tf

--- 02-bastion.tf
+++ 02-bastion.tf
@@ -8,9 +8,14 @@ data "aws_ami" "amazon_linux" {
   owners = ["amazon"]
 }
 
-
 resource "aws_instance" "bastion" {
-  ami = data.aws_ami.amazon_linux.id
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "${local.prefix}-bastion"
+  }
+
 }
+
 ```
